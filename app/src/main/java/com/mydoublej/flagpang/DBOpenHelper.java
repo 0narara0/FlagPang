b9a43ed8935151e183c3552603c30b4363a92d90
in6802 <in6802@gmail.com>
2018-11-27 16:13:44 +0900

    db - JI

--- app/src/main/java/com/mydoublej/flagpang/DBOpenHelper.java
+++ app/src/main/java/com/mydoublej/flagpang/DBOpenHelper.java
@@ -54,8 +54,9 @@ public class DBOpenHelper extends SQLiteOpenHelper {
 //        sql = "INSERT INTO flag " +
 //                "VALUES(null, 'austria', 'oceania', 'austria.png','1')";
 //        db.execSQL(sql);
-        sql = "INSERT INTO flag " +
-                "VALUES(null, 'argentina', 'samerica', 'argentina.png','1')";
+
+        //asia
+        sql = "INSERT INTO flag VALUES(null, 'argentina', 'samerica', 'argentina.png','1')";
         db.execSQL(sql);
     }
 
